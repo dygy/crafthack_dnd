f045c617543b6fb32a9e4035c97a575fedce4321
keddad <keddad@yandex.ru>
2019-08-10 15:05:48 +0300

    Switched to Camelcase

--- models/action.js
+++ models/action.js
@@ -3,8 +3,8 @@ let mod_event = require("./mod_event.js");
 module.exports = () => class Action {
     id: Number;
     name: String;
-    icon_url: String;
-    possible_targets: 'player|mob';
+    iconUrl: String;
+    possibleTargets: 'player|mob';
     effect: mod_event;
     description: String;
 };

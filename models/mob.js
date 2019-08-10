d751db8af56cbfd5255f23b20781f7253cd69826
keddad <keddad@yandex.ru>
2019-08-10 15:07:57 +0300

    Implemented Mob

---
+++ models/mob.js
@@ -0,0 +1,9 @@
+module.exports = () => class Mob{
+    dangerLevel: Number; // Показатель опасности
+    masterBonus: Number;
+    armorClass: Number;
+    hits: Number;
+    attackBouns: Number;
+    roundDamage: Number;
+    saverollPower: Number;
+};
